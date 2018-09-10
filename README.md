8b6adcd55786987b7cb383f0fe1000c0f54ece5b
Vina Fadriani Effendi <effendivina@gmail.com>
2018-09-10 23:37:59 +0700

    final

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # LanguageModelling-NLP
+
+Disusun oleh:
+Vina Fadriani Effendi (1301154560)
+
+Run Program: 
+Jalankan Program pada Jupyter Notebook dan pastikan data set telah berada pada satu file yang sama dengan file .ipynb
--- test.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open('artikel.csv', encoding="utf-8") as csvfile:
-    rawArticles = csv.reader(csvfile, delimiter=',')
-    articles = []
-    for row in rawArticles:
-        articles.append(row[2])
-
-print(articles)
